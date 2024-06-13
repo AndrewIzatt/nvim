993b6a657fac229a4125611a37ee2e4ce6fdcfea
AndrewIzatt <izatt.andrew@gmail.com>
2024-06-13 09:59:09 -0600

    lint code

--- lua/custom/lazy.lua
+++ lua/custom/lazy.lua
@@ -11,21 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ 
-  { 
-   import = "custom.plugins" 
-  }, 
-  { 
-   import = "custom.plugins.lsp" 
-  }
-}, 
-  {
-    checker = {
-      enabled = true,
-      notify = false,
-    },
-    change_detection = {
-      notify = false, -- suppresses change dected message at bottom
-    },
+require("lazy").setup({ { import = "josean.plugins" }, { import = "josean.plugins.lsp" } }, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false, -- suppresses change dected message at bottom
+  },
 })
-
